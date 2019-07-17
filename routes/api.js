7a9dbd258bb2c165bde54a819f79972f4bd31910
SL477 <fishwickt@gmail.com>
2019-07-17 22:02:14 +0100

    Create and update routes complete
    Just deletes and tests to go

--- routes/api.js
+++ routes/api.js
@@ -21,19 +21,56 @@ var bookSchema = new Mongoose.Schema({
   comments: { type: [String] }
 });
 var bookObject = Mongoose.model('bookRecord', bookSchema);
-
+var bookrecord = Mongoose.model('bookrecords', bookSchema);
 module.exports = function (app) {
   Mongoose.connect(process.env.DB, { useNewUrlParser: true }, function (err) {
     if (err) {
       console.log(err);
       res.send(err);
     } else {
+      console.log('db connected');
     }});
 
   app.route('/api/books')
     .get(function (req, res){
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+      //bookObject.find({}).exec(function (err, docs) {
+        //bookObject.aggregate([{"$group": {'_id': '_id', 'title': 'book_title', "commentcount": {$sum: {$size: "$comments"}}}}]).exec(function (err, docs) {
+          /*bookObject.aggregate([
+          {'$group': 
+            {
+              '_id': '$_id',
+              'title': '$book_title', 
+              'commentcount': {'$sum': {'$size': '$comments'}}
+            }},
+            {
+              '$project': {'book_title': '$bookObject.book_title', '_id': '$bookObject._id', 'comments': '$bookObject.comments'}
+            }
+          
+        ]).exec(function (err, docs) {*/
+      bookrecord.find({'book_title': { $ne: null }}).select('_id book_title comments').exec(function (err, docs) {
+        if (err) {
+          console.log(err);
+          res.send(err);
+        } else {
+          var retArr = [];
+          docs.forEach(function (d){
+            var ret = {};
+            //console.log(d.book_title);
+            ret['_id'] = d._id;
+            ret['title'] = d.book_title;
+            ret['commentcount'] = d.comments == undefined? '0': d.comments.length;
+            retArr.push(ret);
+          });
+          //console.log(docs);
+          //console.log({"_id": docs._id, 'title': docs.book_title, 'commentcount': docs.comments == undefined? '0': docs.comments.length});
+          console.log(retArr);
+          //res.json({"_id": docs._id, 'title': docs.book_title, 'commentcount': docs.comments == undefined? '0': docs.comments.length});
+          res.send(retArr);
+          //res.send('hi');
+        }
+      });
     })
     
     .post(function (req, res){
@@ -59,12 +96,33 @@ module.exports = function (app) {
     .get(function (req, res){
       var bookid = req.params.id;
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+      bookrecord.findOne({'_id': bookid}).select('_id book_title comments').exec(function (err, docs) {
+        if (err) {
+          console.log(err);
+          res.send(err);
+        } else {
+          
+          res.json({"_id": docs._id, 'title': docs.book_title, 'comments': docs.comments });
+        }
+      });
     })
     
     .post(function(req, res){
       var bookid = req.params.id;
       var comment = req.body.comment;
       //json res format same as .get
+      bookObject.findByIdAndUpdate({'_id': bookid},{'$push': {'comments':comment}},function (err, docs) {
+        if (err) {
+          console.log(err);
+          res.send(err);
+        } else {
+          console.log(docs._id);
+          console.log(docs.book_title);
+          //console.log(docs.comments);
+          //res.json({"_id": docs._id, 'title': docs.book_title});
+          res.json({"_id": docs._id, 'title': docs.book_title, 'comments': docs.comments });
+        }
+      });
     })
     
     .delete(function(req, res){
