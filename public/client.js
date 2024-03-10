1847b70118f1f393ab35f5680ab8464a132a5817
Tom Fishwick <51207735+SL477@users.noreply.github.com>
2024-03-10 17:59:27 +0000

    Update dependencies (#1)
    * Started updating files
    
    * Got functional tests to run
    
    * Update api.js
    
    * Updated HTML and JS

--- public/client.js
+++ public/client.js
@@ -1,85 +1,152 @@
-$( document ).ready(function() {
-  var items = [];
-  var itemsRaw = [];
-  
-  $.getJSON('/api/books', function(data) {
-    //var items = [];
-    itemsRaw = data;
-    $.each(data, function(i, val) {
-      items.push('<li class="bookItem" id="' + i + '">' + val.title + ' - ' + val.commentcount + ' comments</li>');
-      return ( i !== 14 );
-    });
-    if (items.length >= 15) {
-      items.push('<p>...and '+ (data.length - 15)+' more!</p>');
+/**
+ * @param formID{string}
+ * @returns {object}
+ */
+function getFormData(formID) {
+    // eslint-disable-next-line no-undef
+    const formElement = document.getElementById(formID);
+    if (formElement) {
+        const data = new FormData(formElement);
+        return Object.fromEntries(data);
     }
-    $('<ul/>', {
-      'class': 'listWrapper',
-      html: items.join('')
-      }).appendTo('#display');
-  });
-  
-  var comments = [];
-  $('#display').on('click','li.bookItem',function() {
-    $("#detailTitle").html('<b>'+itemsRaw[this.id].title+'</b> (id: '+itemsRaw[this.id]._id+')');
-    $.getJSON('/api/books/'+itemsRaw[this.id]._id, function(data) {
-      comments = [];
-      $.each(data.comments, function(i, val) {
-        comments.push('<li>' +val+ '</li>');
-      });
-      comments.push('<br><form id="newCommentForm"><input style="width:300px" type="text" class="form-control" id="commentToAdd" name="comment" placeholder="New Comment"></form>');
-      comments.push('<br><button class="btn btn-info addComment" id="'+ data._id+'">Add Comment</button>');
-      comments.push('<button class="btn btn-danger deleteBook" id="'+ data._id+'">Delete Book</button>');
-      $('#detailComments').html(comments.join(''));
+    return {};
+}
+
+/**
+ * Post a comment
+ */
+// eslint-disable-next-line no-unused-vars
+function commentTestFunc() {
+    const commentData = getFormData('commentTest');
+    if (commentData['id'] && commentData['comment']) {
+        fetch('/api/books/' + commentData['id'], {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ comment: commentData['comment'] }),
+            // eslint-disable-next-line no-undef
+        }).then(document.location.reload());
+    }
+    return false;
+}
+
+let itemsRaw = [];
+let items = [];
+
+fetch('/api/books')
+    .then((data) => data.json())
+    .then((data) => {
+        itemsRaw = data;
+        items = data.map((val, i) => {
+            if (i < 14) {
+                return `<li class="bookItem" id="${i}" onclick="bookItemClick('${i}')">${val.title} - ${val.commentcount} comments</li>`;
+            } else if (i === 15) {
+                return `<p>...and ${data.length - 15} more!</p>`;
+            } else {
+                return '';
+            }
+        });
+
+        // eslint-disable-next-line no-undef
+        const display = document.getElementById('display');
+        display.innerHTML =
+            '<ul class="listWrapper">' + items.join('') + '</ul>';
     });
-  });
-  
-  $('#bookDetail').on('click','button.deleteBook',function() {
-    $.ajax({
-      url: '/api/books/'+this.id,
-      type: 'delete',
-      success: function(data) {
-        //update list
-        $('#detailComments').html('<p style="color: red;">'+data+'<p><p>Refresh the page</p>');
-      }
+
+let comments = [];
+// eslint-disable-next-line no-unused-vars
+function bookItemClick(id) {
+    // eslint-disable-next-line no-undef
+    const detailTitle = document.getElementById('detailTitle');
+    detailTitle.innerHTML = `<b>${itemsRaw[id].title}</b> (id: ${itemsRaw[id]._id})`;
+
+    fetch('/api/books/' + itemsRaw[id]._id)
+        .then((data) => data.json())
+        .then((data) => {
+            console.log(data);
+            comments = data.comments.map((val) => {
+                if (val) {
+                    return `<li>${val}</li>`;
+                }
+                return '';
+            });
+            comments.push(
+                '<br><form id="newCommentForm"><input style="width:300px" type="text" class="form-control" id="commentToAdd" name="comment" placeholder="New Comment"></form>'
+            );
+            comments.push(
+                `<br><button class="btn btn-info addComment" id="${data._id}" onclick="addCommentClick('${data._id}')">Add Comment</button>`
+            );
+            comments.push(
+                `<button class="btn btn-danger deleteBook" id="${data._id}" onclick="deleteBookClick('${data._id}')">Delete Book</button>`
+            );
+            // eslint-disable-next-line no-undef
+            const detailComments = document.getElementById('detailComments');
+            detailComments.innerHTML = comments.join('');
+        });
+}
+
+// eslint-disable-next-line no-unused-vars
+function deleteBookClick(id) {
+    fetch('/api/books/' + id, {
+        method: 'DELETE',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+    })
+        .then((data) => data.text())
+        .then((data) => {
+            // update list
+            // eslint-disable-next-line no-undef
+            const detailComments = document.getElementById('detailComments');
+            detailComments.innerHTML = `<p style="color: red;">${data}<p><p>Refresh the page</p>`;
+        });
+}
+
+// eslint-disable-next-line no-unused-vars
+function addCommentClick(id) {
+    // eslint-disable-next-line no-undef
+    const commentToAdd = document.getElementById('commentToAdd');
+    const newComment = commentToAdd.value;
+    fetch('/api/books/' + id, {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(getFormData('newCommentForm')),
+    }).then(() => {
+        comments.unshift(newComment); // adds new comment to top of list
+        // eslint-disable-next-line no-undef
+        const detailComments = document.getElementById('detailComments');
+        detailComments.innerHTML = comments.join('');
     });
-  });  
-  
-  $('#bookDetail').on('click','button.addComment',function() {
-    var newComment = $('#commentToAdd').val();
-    $.ajax({
-      url: '/api/books/'+this.id,
-      type: 'post',
-      dataType: 'json',
-      data: $('#newCommentForm').serialize(),
-      success: function(data) {
-        comments.unshift(newComment); //adds new comment to top of list
-        $('#detailComments').html(comments.join(''));
-      }
-    });
-  });
-  
-  $('#newBook').click(function() {
-    $.ajax({
-      url: '/api/books',
-      type: 'post',
-      dataType: 'json',
-      data: $('#newBookForm').serialize(),
-      success: function(data) {
-        //update list
-      }
-    });
-  });
-  
-  $('#deleteAllBooks').click(function() {
-    $.ajax({
-      url: '/api/books',
-      type: 'delete',
-      dataType: 'json',
-      data: $('#newBookForm').serialize(),
-      success: function(data) {
-        //update list
-      }
-    });
-  }); 
-  
-});
+}
+
+// eslint-disable-next-line no-unused-vars
+function newBookFormFunc() {
+    fetch('/api/books', {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(getFormData('newBookForm')),
+        // eslint-disable-next-line no-undef
+    }).then(document.location.reload());
+    return false;
+}
+
+// eslint-disable-next-line no-unused-vars
+function deleteAllBooks() {
+    fetch('/api/books', {
+        method: 'DELETE',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+        // eslint-disable-next-line no-undef
+    }).then(document.location.reload());
+}
--- server.js
+++ server.js
@@ -1,63 +1,63 @@
 'use strict';
 
-var express     = require('express');
-var bodyParser  = require('body-parser');
-var cors        = require('cors');
+import express from 'express';
+import pkg from 'body-parser';
+const { json, urlencoded } = pkg;
+import cors from 'cors';
+import { config } from 'dotenv';
+import apiRoutes from './routes/api.js';
+import fccTestingRoutes from './routes/fcctesting.js';
+import runner from './test-runner.js';
+import helmet, { hidePoweredBy } from 'helmet';
 
-var apiRoutes         = require('./routes/api.js');
-var fccTestingRoutes  = require('./routes/fcctesting.js');
-var runner            = require('./test-runner');
-
-const helmet = require('helmet');
-
-var app = express();
+config();
+const app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.use(cors({origin: '*'})); //USED FOR FCC TESTING PURPOSES ONLY!
+app.use(cors({ origin: '*' })); //USED FOR FCC TESTING PURPOSES ONLY!
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(json());
+app.use(urlencoded({ extended: true }));
 
-//Helmet
-app.use(helmet());
-app.use(helmet.hidePoweredBy({ setTo: 'PHP 4.2.0' }));
-app.use(helmet.noCache());
+// Helmet
+app.use(helmet({ contentSecurityPolicy: false }));
+app.use(hidePoweredBy({ setTo: 'PHP 4.2.0' }));
+// app.use(noCache());
 
-//Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
+// Index page (static HTML)
+app.route('/').get(function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
-  });
+});
 
 //For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
-apiRoutes(app);  
-    
+//Routing for API
+apiRoutes(app);
+
 //404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
+// eslint-disable-next-line no-unused-vars
+app.use(function (req, res, next) {
+    res.status(404).type('text').send('Not Found');
 });
 
 //Start our server and tests!
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Listening on port " + process.env.PORT);
-  if(process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
-    setTimeout(function () {
-      try {
-        runner.run();
-      } catch(e) {
-        var error = e;
-          console.log('Tests are not valid:');
-          console.log(error);
-      }
-    }, 1500);
-  }
+const listener = app.listen(process.env.PORT || 3000, function () {
+    console.log('Listening on port ' + listener.address().port);
+    if (process.env.NODE_ENV === 'test') {
+        console.log('Running Tests...');
+        setTimeout(function () {
+            try {
+                runner.run();
+                console.log('Run tests');
+            } catch (e) {
+                var error = e;
+                console.log('Tests are not valid:');
+                console.log(error);
+            }
+        }, 1500);
+    }
 });
 
-module.exports = app; //for unit/functional testing
+export default app; //for unit/functional testing
